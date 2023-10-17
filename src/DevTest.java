bd0d48502af194772630528ef76fec16297f4549
eghearn <eghearn05@gmail.com>
2023-10-17 15:01:01 -0400

    tc

--- src/DevTest.java
+++ src/DevTest.java
@@ -8,15 +8,29 @@ public class DevTest
     {
         String firstName = "";
         int age = 0;
+        double salary = 0;
+        int favNum = 0;
 
         /*
 
         firstName = SafeInput.getNonZeroLenString(testScan, "Enter your first name");
         System.out.println("Firstname is " + firstName);
-         */
 
         age = getInt(testScan, "Enter your age");
         System.out.println("You said your age is: " + age);
+
+        salary = getDouble(testScan, "Enter your salary: ");
+        System.out.println("You said salary is: " + salary);
+
+        favNum = getRangedInt(testScan, "Enter your favorite number", 1, 10);
+        System.out.println("You said your fav num is: " + favNum);
+         */
+
+
+
+
+
+
     }
     // methods go after the main!
 
@@ -75,6 +89,76 @@ public class DevTest
 
         return value;
     }
+
+    /**
+     * Returns an unconstrained double value
+     * @param pipe scanner
+     * @param prompt
+     * @return
+     */
+    public static double getDouble(Scanner pipe, String prompt)
+    {
+        boolean done = false;
+        String trash = "";
+        double value = 0;
+        do
+        {
+            System.out.print(prompt + ": ");
+            if(pipe.hasNextDouble())
+            {
+                value = pipe.nextDouble();
+                pipe.nextLine(); // clear the buffer
+                done = true;
+            }
+            else
+            {
+                trash = pipe.nextLine();
+                System.out.println("\nYou must enter a number. You entered: " + trash);
+            }
+
+        }while(!done);
+
+        return value;
+    }
+
+    /**
+     * gets int val from the user with a high and low constraint
+     * @param pipe scanner to use for input
+     * @param prompt msg to user for what to enter
+     * @param low Bottom value for inclusive range
+     * @param high Top value for inclusive range
+     * @return The value
+     */
+    public static int getRangedInt(Scanner pipe, String prompt, int low, int high)
+    {
+        boolean done = false;
+        String trash = "";
+        int value = 0;
+        do
+        {
+            System.out.print(prompt + " [" + low + " - " + high + "]: ");
+            if(pipe.hasNextInt())
+            {
+                value = pipe.nextInt();
+                pipe.nextLine(); // clear the buffer
+
+                if(value >=low && value <= high)
+                    done = true;
+                else
+                    System.out.println("\nYou must enter a value within the range [" + low + " - " + high + "]: ");
+            }
+            else
+            {
+                trash = pipe.nextLine();
+                System.out.println("\nYou must enter a number. You entered: " + trash);
+            }
+
+        }while(!done);
+
+        return value;
+    }
+
+
 
 
 }
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -60,4 +60,43 @@ public class SafeInput
     }
 
 
+    // BROKEN SALARY GOES HERE
+
+    /**
+     * gets int val from the user with a high and low constraint
+     * @param pipe scanner to use for input
+     * @param prompt msg to user for what to enter
+     * @param low Bottom value for inclusive range
+     * @param high Top value for inclusive range
+     * @return The value
+     */
+    public static int getRangedInt(Scanner pipe, String prompt, int low, int high)
+    {
+        boolean done = false;
+        String trash = "";
+        int value = 0;
+        do
+        {
+            System.out.print(prompt + " [" + low + " - " + high + "]: ");
+            if(pipe.hasNextInt())
+            {
+                value = pipe.nextInt();
+                pipe.nextLine(); // clear the buffer
+
+                if(value >=low && value <= high)
+                    done = true;
+                else
+                    System.out.println("\nYou must enter a value within the range [" + low + " - " + high + "]: ");
+            }
+            else
+            {
+                trash = pipe.nextLine();
+                System.out.println("\nYou must enter a number. You entered: " + trash);
+            }
+
+        }while(!done);
+
+        return value;
+
+
 }
