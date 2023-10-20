5376ce656dad6d057a28bf21140ccdaac77c51b3
eghearn <eghearn05@gmail.com>
2023-10-20 13:26:17 -0400

    tc

---
+++ src/BirthDateTime.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+public class BirthDateTime
+{
+    public static Scanner testScan = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        int year = 0;
+        int day = 0;
+        int month = 0;
+        boolean done = false;
+
+
+        year = SafeInput.getRangedInt(testScan, "Enter the year you were born", 1950, 2015);
+        System.out.println("You said your year of birth is: " + year);
+        month = SafeInput.getRangedInt(testScan, "Enter the month you were born", 1, 12);
+        System.out.println("You said your month of birth is: " + month);
+
+
+            switch(month)
+            {
+                case 1:
+                    day = SafeInput.getRangedInt(testScan, "Enter the day you were born", 1, 31);
+                    System.out.println("You said the day you were born is: " + day);
+                    break;
+                case 2:
+                    day = SafeInput.getRangedInt(testScan, "Enter the day you were born", 1, 28);
+                    System.out.println("You said the day you were born is: " + day);
+                    break;
+                case 3:
+                    day = SafeInput.getRangedInt(testScan, "Enter the day you were born", 1, 31);
+                    System.out.println("You said the day you were born is: " + day);
+                    break;
+                case 4:
+                    day = SafeInput.getRangedInt(testScan, "Enter the day you were born", 1, 30);
+                    System.out.println("You said the day you were born is: " + day);
+                    break;
+                case 5:
+                    day = SafeInput.getRangedInt(testScan, "Enter the day you were born", 1, 31);
+                    System.out.println("You said the day you were born is: " + day);
+                    break;
+                case 6:
+                    day = SafeInput.getRangedInt(testScan, "Enter the day you were born", 1, 30);
+                    System.out.println("You said the day you were born is: " + day);
+                    break;
+                case 7:
+                    day = SafeInput.getRangedInt(testScan, "Enter the day you were born", 1, 31);
+                    System.out.println("You said the day you were born is: " + day);
+                    break;
+                case 8:
+                    day = SafeInput.getRangedInt(testScan, "Enter the day you were born", 1, 31);
+                    System.out.println("You said the day you were born is: " + day);
+                    break;
+                case 9:
+                    day = SafeInput.getRangedInt(testScan, "Enter the day you were born", 1, 30);
+                    System.out.println("You said the day you were born is: " + day);
+                    break;
+                case 10:
+                    day = SafeInput.getRangedInt(testScan, "Enter the day you were born", 1, 31);
+                    System.out.println("You said the day you were born is: " + day);
+                    break;
+                case 11:
+                    day = SafeInput.getRangedInt(testScan, "Enter the day you were born", 1, 30);
+                    System.out.println("You said the day you were born is: " + day);
+                    break;
+                case 12:
+                    day = SafeInput.getRangedInt(testScan, "Enter the day you were born", 1, 31);
+                    System.out.println("You said the day you were born is: " + day);
+                    break;
+            }
+
+        System.out.println("You were born on " + month + " / " + day + " / " + year);
+
+
+
+    }
+}
